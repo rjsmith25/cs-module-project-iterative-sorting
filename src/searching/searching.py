68f797c1121522300b20ca52d6c065d0c6cf4b7a
Russell Smith <russellsmith@MacBook-Pro.local>
2020-09-02 21:14:01 -0400

    completed mvp iterative sorting

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,20 @@
 def linear_search(arr, target):
-    # Your code here
-
-
+    for x in range(0,len(arr)):
+        if target == arr[x]:
+            return x
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
-    # Your code here
-
-
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (high + low) // 2
+        if target == arr[mid]:
+            return mid
+        if target < arr[mid]:
+            high = mid - 1
+        if target > arr[mid]:
+            low = mid + 1
     return -1  # not found
